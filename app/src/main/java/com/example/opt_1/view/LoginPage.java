daa4e23b654c63ae19a28ba4632d2c27a397cf0d
KasparT <kaspar.tullusgmail.com>
2023-09-05 07:38:03 +0300

    Screen pages functions and logic (not done)

--- app/src/main/java/com/example/opt_1/view/LoginPage.java
+++ app/src/main/java/com/example/opt_1/view/LoginPage.java
@@ -1,11 +1,13 @@
 package com.example.opt_1.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.opt_1.R;
 import com.example.opt_1.control.Controller;
@@ -14,24 +16,38 @@ import com.example.opt_1.control.IViewtoModel;
 
 public class LoginPage extends AppCompatActivity {
 
-    private EditText usernameField;
-    private EditText passwordField;
-    private Button loginButton;
+    private EditText usernameField, passwordField;
+    private Button loginButton, registerButton;
+
     private IViewtoModel controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         controller = new Controller();
         setContentView(R.layout.login_page);
+
         loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+
+        //LoginPage Fields
         usernameField = findViewById(R.id.loginUserNameInput);
         passwordField = findViewById(R.id.loginPasswordInput);
+
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                controller.getLoginInfo(usernameField.getText().toString(),passwordField.getText().toString());
+                controller.getLoginInfo(usernameField.getText().toString(), passwordField.getText().toString());
             }
         });
 
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, RegisterPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
