7b4fce01850e6851828bddbdc19d80666876d60e
RauliYoung <tristan.ellenberg@gmail.com>
2023-09-07 19:40:18 +0300

    removed database schite to dao

--- app/src/main/java/com/example/opt_1/model/DAO.java
+++ app/src/main/java/com/example/opt_1/model/DAO.java
@@ -1,17 +1,22 @@
 package com.example.opt_1.model;
-
-import java.sql.SQLException;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DAO implements IDAO{
 
 
     @Override
-    public void createData(int userid) throws SQLException {
-
+    public void createData(int userid){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 
     @Override
-    public void updateData() throws SQLException {
-
+    public void updateData(){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
     }
 }
+
+//Example for putting users to collection.
+     /*   db.collection("users")
+                .add(user)
+                .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
+                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e))*/;
