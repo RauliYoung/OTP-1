b3002c71401451385ec9fe0ce258cd44eb14e78b
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2023-09-07 19:08:11 +0300

    nonnii se meni sinne

--- app/src/main/java/com/example/opt_1/model/User.java
+++ app/src/main/java/com/example/opt_1/model/User.java
@@ -5,13 +5,15 @@ public class User {
     private final int userID;
     private String firstName;
     private String lastName;
+    private String username;
     private String email;
     private String password;
 
-    public User(int userID, String firstName, String lastName, String email, String password){
+    public User(int userID, String firstName, String lastName, String username, String email, String password){
         this.userID = userID;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.username = username;
         this.email = email;
         this.password = password;
     }
@@ -35,6 +37,9 @@ public class User {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getUsername(){return username;}
+    public void setUsername(String username){this.username = username;}
 
     public String getEmail() {
         return email;
