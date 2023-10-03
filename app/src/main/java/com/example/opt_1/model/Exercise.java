aa5e48fc8ccb1c43976219f7f2d53b09fb83111a
SmuuSka <samuaikio@gmail.com>
2023-10-03 12:42:31 +0300

    Group broken still, date works

--- app/src/main/java/com/example/opt_1/model/Exercise.java
+++ app/src/main/java/com/example/opt_1/model/Exercise.java
@@ -2,16 +2,29 @@ package com.example.opt_1.model;
 
 import java.time.LocalDateTime;
 import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Exercise implements IExercise {
     private double exerciseTime;
     private double exerciseInMeters;
     private double avgSpeed;
+    private String exerciseDate;
 
     public Exercise(double exerciseTime, double exerciseInMeters, double avgSpeed){
         this.exerciseTime = exerciseTime;
         this.exerciseInMeters = exerciseInMeters;
         this.avgSpeed = avgSpeed;
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        this.exerciseDate = date.format(format);
+    }
+
+    public String getExerciseDate() {
+        return exerciseDate;
+    }
+
+    public void setExerciseDate(String exerciseDate) {
+        this.exerciseDate = exerciseDate;
     }
 
     public double getExerciseTime() {
