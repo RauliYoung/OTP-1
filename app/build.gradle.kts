5963037ac5f5086dcff11dce4b976cef27a360c8
KasparT <kaspar.tullusgmail.com>
2023-11-02 00:48:57 +0200

    UI Test -Logging in DONE

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,11 +51,16 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.2.3"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-
+    implementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    implementation("androidx.test:monitor:1.6.1")
+    androidTestImplementation("androidx.test:runner:1.4.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     implementation ("com.firebaseui:firebase-ui-auth:7.2.0")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation ("androidx.test:runner:1.5.2")
+    androidTestImplementation ("androidx.test:rules:1.5.0")
+
     testImplementation ("org.mockito:mockito-core:5.6.0")
 }
 tasks.withType<Test> {
