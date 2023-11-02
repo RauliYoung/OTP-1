c3513fe2cb3fb54fe4cb98903992ab8c4286e878
KasparT <kaspar.tullusgmail.com>
2023-11-02 07:31:41 +0200

    Fix?

--- app/src/androidTest/java/LoginPageUITest.java
+++ app/src/androidTest/java/LoginPageUITest.java
@@ -7,15 +7,15 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.example.opt_1.R;
 import com.example.opt_1.view.LoginPage;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+@RunWith(AndroidJUnit4.class)
 public class LoginPageUITest {
 
     @Rule
