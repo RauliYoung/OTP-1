f56ba3c313447fe0b4b5f0f72e8daed097fba790
KasparT <kaspar.tullusgmail.com>
2023-11-08 21:24:43 +0200

    Dropdown Language list on Loginpage, changes the whole app language.

--- app/src/main/java/com/example/opt_1/view/LoginPage.java
+++ app/src/main/java/com/example/opt_1/view/LoginPage.java
@@ -1,13 +1,17 @@
 package com.example.opt_1.view;
 
-
-import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
-import android.telephony.TelephonyManager;
+import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,6 +22,8 @@ import com.example.opt_1.model.CRUDCallbacks;
 import com.example.opt_1.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Locale;
 
 
 public class LoginPage extends AppCompatActivity {
@@ -30,6 +36,7 @@ public class LoginPage extends AppCompatActivity {
 
     private FirebaseAuth auth = FirebaseAuth.getInstance();
     private FirebaseUser fireUser = auth.getCurrentUser();
+    private boolean languageChanged;
     FirebaseAuth.AuthStateListener authlistener;
 
     @Override
@@ -37,6 +44,77 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         controller = new Controller();
         setContentView(R.layout.login_page);
+
+        //Language Selector Spinner.
+
+        Spinner languageSpinner = findViewById(R.id.languageSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.languages_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(adapter);
+
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                    String selectedLanguage = parentView.getItemAtPosition(position).toString();
+
+                    String languageCode = "";
+                    Locale locale = null;
+                    switch (selectedLanguage) {
+                        case "English":
+                            locale = new Locale("en");
+                            languageCode = "en";
+                            break;
+
+                        case "Estonia":
+                            locale = new Locale("et");
+                            languageCode = "et";
+                            break;
+
+                        case "Swedish":
+                            locale = new Locale("sv");
+                            languageCode = "sv";
+                            break;
+
+                        case "Suomi":
+                            locale = new Locale("fi");
+                            languageCode = "fi";
+                            break;
+
+                        case "Persian":
+                            locale = new Locale("fa");
+                            languageCode = "fa";
+                            break;
+
+                        default:
+                            locale = Locale.getDefault();
+                            break;
+                    }
+
+                Locale newLocale = new Locale(languageCode);
+                Locale currentLocale = getResources().getConfiguration().locale;
+
+                if (!currentLocale.getLanguage().equals(languageCode)) {
+                    Locale.setDefault(newLocale);
+                    Configuration config = new Configuration();
+                    config.locale = newLocale;
+
+                    Resources resources = getResources();
+                    resources.updateConfiguration(config, resources.getDisplayMetrics());
+                    recreate();
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+
+            }
+
+        });
+
+
+
         if(auth.getCurrentUser() != null){
             auth.signOut();
             System.out.println("Login page user: " + auth.getCurrentUser());
@@ -61,6 +139,8 @@ public class LoginPage extends AppCompatActivity {
             }
         });
     }
+
+
     private void userLogin(){
         controller.userLogin(usernameField.getText().toString(), passwordField.getText().toString(), new CRUDCallbacks() {
             @Override
