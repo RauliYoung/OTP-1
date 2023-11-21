5a9ce3a53b01b02961569ca631369c49bd3a36ce
SmuuSka <samuaikio@gmail.com>
2023-11-21 19:06:57 +0200

    new unit test

--- app/src/test/java/com/example/opt_1/model/LocationTrackerTest.java
+++ app/src/test/java/com/example/opt_1/model/LocationTrackerTest.java
@@ -3,8 +3,10 @@ package com.example.opt_1.model;
 import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(BlockJUnit4ClassRunner.class)
 public class LocationTrackerTest {
@@ -16,14 +18,14 @@ public class LocationTrackerTest {
     }
 
     @Test
-    public void calculateDistanceWithCorrectCoordinates() {
+    public void calculateDistance() {
         assertEquals("Should calculate the distance between two locations", 266.2,
-                locationTracker.calculateDistance(60.21597801314619, 24.905964043814574, 60.2177206661023, 24.90265956238168), DELTA);
+                LocationTracker.calculateDistance(60.21597801314619, 24.905964043814574, 60.2177206661023, 24.90265956238168), DELTA);
     }
 
     @Test
-    public void calculateDistanceWithIncorrectCoordinates() {
+    public void calculateDistance_incorrect_coordinates() {
         assertNotEquals("Should calculate the distance between two locations", 266.2,
-                locationTracker.calculateDistance(59.21597801314619, 24.905964043814574, 60.2177206661023, 24.90265956238168), DELTA);
+                LocationTracker.calculateDistance(59.21597801314619, 24.905964043814574, 60.2177206661023, 24.90265956238168), DELTA);
     }
 }
