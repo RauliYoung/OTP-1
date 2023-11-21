15fa04f2c90ecc0d038aee607da2ed53c2007d49
SmuuSka <samuaikio@gmail.com>
2023-11-21 19:06:57 +0200

    new unit test

--- app/src/test/java/com/example/opt_1/model/LocationTrackerTest.java
+++ app/src/test/java/com/example/opt_1/model/LocationTrackerTest.java
@@ -2,15 +2,24 @@ package com.example.opt_1.model;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.internal.runners.JUnit4ClassRunner;
 import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
 import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(BlockJUnit4ClassRunner.class)
 public class LocationTrackerTest {
     double DELTA = 0.1;
 
     @Test
     public void calculateDistance() {
-        assertEquals("Should calculate the distance between two locations", 266.2, LocationTracker.calculateDistance(60.21597801314619, 24.905964043814574, 60.2177206661023, 24.90265956238168), DELTA);
+        assertEquals("Should calculate the distance between two locations", 266.2,
+                LocationTracker.calculateDistance(60.21597801314619, 24.905964043814574, 60.2177206661023, 24.90265956238168), DELTA);
+    }
+
+    @Test
+    public void calculateDistance_incorrect_coordinates() {
+        assertNotEquals("Should calculate the distance between two locations", 266.2,
+                LocationTracker.calculateDistance(59.21597801314619, 24.905964043814574, 60.2177206661023, 24.90265956238168), DELTA);
     }
 }
