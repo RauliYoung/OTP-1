15a39e28b16ca244fe206228e6748a9a41889c35
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2023-11-26 13:13:09 +0200

    login testit korjattu kaikille kielille

--- app/src/androidTest/java/com/example/opt_1/view/LoginTestSwedish.java
+++ app/src/androidTest/java/com/example/opt_1/view/LoginTestSwedish.java
@@ -1,108 +1,126 @@
 package com.example.opt_1.view;
 
 
-import androidx.test.espresso.DataInteraction;
-import androidx.test.espresso.ViewInteraction;
-import androidx.test.filters.LargeTest;
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import static androidx.test.InstrumentationRegistry.getInstrumentation;
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.pressBack;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
-import static androidx.test.espresso.action.ViewActions.*;
-import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static androidx.test.espresso.matcher.ViewMatchers.*;
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 import com.example.opt_1.R;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.anything;
-import static org.hamcrest.Matchers.is;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class LoginTestSwedish {
 
+
+    private String test_user;
+    private String test_pass;
+    private final int timeout = 5000;
+
+    @Before
+    public void init() throws PackageManager.NameNotFoundException {
+        ApplicationInfo ai = getApplicationContext().getPackageManager()
+                .getApplicationInfo(getApplicationContext().getPackageName(), PackageManager.GET_META_DATA);
+
+        test_user = (String) ai.metaData.get("test_username");
+        test_pass = (String) ai.metaData.get("test_password");
+    }
     @Rule
     public ActivityScenarioRule<LoginPage> mActivityScenarioRule =
             new ActivityScenarioRule<>(LoginPage.class);
 
     @Test
-    public void loginTestSwedish() {
+    public void loginTestSwedish() throws InterruptedException {
         ViewInteraction appCompatSpinner = onView(
-allOf(withId(R.id.languageSpinner),
-childAtPosition(
-childAtPosition(
-withId(android.R.id.content),
-0),
-0),
-isDisplayed()));
+                allOf(withId(R.id.languageSpinner),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
         appCompatSpinner.perform(click());
-        
+
         DataInteraction appCompatCheckedTextView = onData(anything())
-.inAdapterView(childAtPosition(
-withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
-0))
-.atPosition(2);
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(2);
         appCompatCheckedTextView.perform(click());
-        
+
         ViewInteraction textInputEditText = onView(
-allOf(withId(R.id.loginUserNameInput),
-childAtPosition(
-childAtPosition(
-withClassName(is("com.google.android.material.textfield.TextInputLayout")),
-0),
-0),
-isDisplayed()));
-        textInputEditText.perform(replaceText("testi@testi.fi"), closeSoftKeyboard());
-        
+                allOf(withId(R.id.loginUserNameInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText.perform(replaceText(test_user), closeSoftKeyboard());
+
         ViewInteraction textInputEditText2 = onView(
-allOf(withId(R.id.loginPasswordInput),
-childAtPosition(
-childAtPosition(
-withClassName(is("com.google.android.material.textfield.TextInputLayout")),
-0),
-0),
-isDisplayed()));
-        textInputEditText2.perform(replaceText("testi123"), closeSoftKeyboard());
-        
+                allOf(withId(R.id.loginPasswordInput),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("com.google.android.material.textfield.TextInputLayout")),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textInputEditText2.perform(replaceText(test_pass), closeSoftKeyboard());
+
         ViewInteraction materialButton = onView(
-allOf(withId(R.id.loginButton), withText("Logga in"),
-childAtPosition(
-allOf(withId(R.id.loginPage),
-childAtPosition(
-withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
-2)),
-4),
-isDisplayed()));
+                allOf(withId(R.id.loginButton), withText("Logga in"),
+                        childAtPosition(
+                                allOf(withId(R.id.loginPage),
+                                        childAtPosition(
+                                                withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
+                                                2)),
+                                4),
+                        isDisplayed()));
         materialButton.perform(click());
-        
+        Thread.sleep(timeout);
+
         ViewInteraction materialButton2 = onView(
-allOf(withId(R.id.logOut_Button), withText("Logga ut"),
-childAtPosition(
-allOf(withId(R.id.fragmentHome),
-childAtPosition(
-withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
-0)),
-7),
-isDisplayed()));
+                allOf(withId(R.id.logOut_Button), withText("Logga ut"),
+                        childAtPosition(
+                                allOf(withId(R.id.fragmentHome),
+                                        childAtPosition(
+                                                withClassName(is("androidx.constraintlayout.widget.ConstraintLayout")),
+                                                0)),
+                                7),
+                        isDisplayed()));
         materialButton2.perform(click());
-        }
-    
+    }
+
     private static Matcher<View> childAtPosition(
             final Matcher<View> parentMatcher, final int position) {
 
@@ -117,8 +135,8 @@ isDisplayed()));
             public boolean matchesSafely(View view) {
                 ViewParent parent = view.getParent();
                 return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup)parent).getChildAt(position));
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
     }
-    }
+}
